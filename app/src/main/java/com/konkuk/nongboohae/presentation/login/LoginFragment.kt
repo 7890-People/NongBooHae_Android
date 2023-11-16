9b399f9ffd443c16da72c67e1111d03a67d91009
ddiyooong <jsshin000107@naver.com>
2023-11-16 14:58:28 +0900

    feat: 로그인 페이지 레이아웃 완성

---
+++ app/src/main/java/com/konkuk/nongboohae/presentation/login/LoginFragment.kt
@@ -0,0 +1,19 @@
+package com.konkuk.nongboohae.presentation.login
+
+import BaseFragment
+import com.konkuk.nongboohae.R
+import com.konkuk.nongboohae.databinding.FragmentLoginBinding
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+    override val TAG: String
+        get() = "LoginFragment"
+    override val layoutRes: Int
+        get() = R.layout.fragment_login
+
+
+    override fun afterViewCreated() {
+
+    }
+
+
+}
