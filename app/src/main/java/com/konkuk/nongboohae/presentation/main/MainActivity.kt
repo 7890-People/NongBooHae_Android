055d6d732262b1701013d6b80e6ea76f5aafd98b
ddiyooong <jsshin000107@naver.com>
2023-11-17 22:42:03 +0900

    feat: MainActivity BottomNavigation 추가

---
+++ app/src/main/java/com/konkuk/nongboohae/presentation/main/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.konkuk.nongboohae.presentation.main
+
+import com.konkuk.nongboohae.R
+import com.konkuk.nongboohae.databinding.ActivityMainBinding
+import com.konkuk.nongboohae.presentation.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override val TAG: String = "MainActivity"
+    override val layoutRes: Int = R.layout.activity_main
+
+    override fun initViewModel() {
+
+    }
+
+    override fun afterViewCreated() {
+
+    }
+
+
+}
