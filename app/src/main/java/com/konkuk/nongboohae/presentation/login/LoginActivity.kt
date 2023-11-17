bead918345b4e631da2173beb99770d2b0d18278
ddiyooong <jsshin000107@naver.com>
2023-11-17 18:14:37 +0900

    feat: BaseActivity ViewModel 구현 함수 추가

--- app/src/main/java/com/konkuk/nongboohae/presentation/login/LoginActivity.kt
+++ app/src/main/java/com/konkuk/nongboohae/presentation/login/LoginActivity.kt
@@ -1,14 +1,21 @@
 package com.konkuk.nongboohae.presentation.login
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.konkuk.nongboohae.R
+import com.konkuk.nongboohae.databinding.ActivityLoginBinding
+import com.konkuk.nongboohae.presentation.base.BaseActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity<ActivityLoginBinding>() {
+    override val TAG: String = "LoginActivity"
+    override val layoutRes: Int = R.layout.activity_login
+    lateinit var viewModel: LoginViewModel
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+    override fun initViewModel() {
+        viewModel = createViewModel(MemberRepository())
     }
+
+    override fun afterViewCreated() {
+
+    }
+
+
 }
