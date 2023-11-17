708e96f84d588c84cbdbf98ef1134782e776f10c
lee-june-young <uhiccup@gmail.com>
2023-11-17 18:09:58 +0900

    design: DiagnosisResultActivity xml

---
+++ app/src/main/java/com/konkuk/nongboohae/presentation/diagnosis/DiagnosisResultActivity.kt
@@ -0,0 +1,13 @@
+package com.konkuk.nongboohae.presentation.diagnosis
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil.setContentView
+import com.konkuk.nongboohae.R
+import com.konkuk.nongboohae.databinding.ActivityDiagnosisResultBinding
+import com.konkuk.nongboohae.presentation.base.BaseActivity
+
+class DiagnosisResultActivity : BaseActivity<ActivityDiagnosisResultBinding, >() {
+
+
+}
