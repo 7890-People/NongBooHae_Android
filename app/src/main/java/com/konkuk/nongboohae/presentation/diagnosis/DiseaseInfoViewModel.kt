50a7688fa33db9cfe24c190842ca5a23d11d7292
lee-june-young <uhiccup@gmail.com>
2023-11-26 12:19:47 +0900

    feat: 질병정보 Activity 구현

---
+++ app/src/main/java/com/konkuk/nongboohae/presentation/diagnosis/DiseaseInfoViewModel.kt
@@ -0,0 +1,7 @@
+package com.konkuk.nongboohae.presentation.diagnosis
+
+import androidx.lifecycle.ViewModel
+
+class DiseaseInfoViewModel(val diseaseRepository: DiseaseRepository):  ViewModel() {
+
+}
