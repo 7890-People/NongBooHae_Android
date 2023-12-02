3242e065dd3944779def4867c4ea6de02f4bfd9f
lee-june-young <uhiccup@gmail.com>
2023-12-02 12:44:58 +0900

    feat: 진단기록 fragment 작업

---
+++ app/src/main/java/com/konkuk/nongboohae/presentation/main/history/HistoryFragment.kt
@@ -0,0 +1,16 @@
+package com.konkuk.nongboohae.presentation.main.history
+
+import BaseFragment
+import com.konkuk.nongboohae.R
+import com.konkuk.nongboohae.databinding.FragmentHistoryBinding
+
+class HistoryFragment : BaseFragment<FragmentHistoryBinding>() {
+    override val TAG: String
+        get() = "HistoryFragment"
+    override val layoutRes: Int
+        get() = R.layout.fragment_history
+
+    override fun afterViewCreated() {
+
+    }
+}
