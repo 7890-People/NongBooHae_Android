3252594267bd5b3272c0619e54ab6a3e0807f6f9
lee-june-young <uhiccup@gmail.com>
2024-01-22 17:31:37 +0900

    feat: DiagnosisBottomSheet->DiagnosisResultActivity로 plantName 전달

--- app/src/main/java/com/konkuk/nongboohae/presentation/diagnosis/DiagnosisResultActivity.kt
+++ app/src/main/java/com/konkuk/nongboohae/presentation/diagnosis/DiagnosisResultActivity.kt
@@ -43,7 +43,7 @@ class DiagnosisResultActivity : BaseActivity<ActivityDiagnosisResultBinding>() {
     private fun initData() {
         photoUri = intent.getStringExtra("photoUri")!!
         val filePath = intent.getStringExtra("currentPhotoPath")
-        plantName = "포도"
+        plantName = intent.getStringExtra("plantName")!!
         Glide.with(this)
             .load(photoUri)
             .into(binding.diagnosisResultPhotoIv)
