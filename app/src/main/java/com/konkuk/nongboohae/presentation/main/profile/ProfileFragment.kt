a233c08dcc9091d61ed06e09c186561cc9f78818
lee-june-young <uhiccup@gmail.com>
2024-01-23 03:29:03 +0900

    feat: ProfileSettingActivity 앱 설정 화면 개발

--- app/src/main/java/com/konkuk/nongboohae/presentation/main/profile/ProfileFragment.kt
+++ app/src/main/java/com/konkuk/nongboohae/presentation/main/profile/ProfileFragment.kt
@@ -1,6 +1,7 @@
 package com.konkuk.nongboohae.presentation.main.profile
 
 import BaseFragment
+import android.content.Intent
 import com.konkuk.nongboohae.R
 import com.konkuk.nongboohae.databinding.FragmentProfileBinding
 
@@ -8,7 +9,14 @@ class ProfileFragment: BaseFragment<FragmentProfileBinding>() {
     override val TAG: String = "ProfileFragment"
     override val layoutRes: Int = R.layout.fragment_profile
     override fun afterViewCreated() {
+        initClickListener()
+    }
 
+    private fun initClickListener() {
+        binding.profileSettingLayout.setOnClickListener {
+            val intent = Intent(requireContext(), ProfileSettingActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
